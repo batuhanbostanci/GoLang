8c695028a9eec9ce4c9b8b1e4a1b311c1e0550b5
batuhanbostanci <batuhanbostanci2@hotmail.com>
2026-01-07 23:20:14 +0300

    Outsourcing some of the packages

--- Section-9 (PractiseProject-Price Calculator)/main.go
+++ Section-9 (PractiseProject-Price Calculator)/main.go
@@ -1,24 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price-calculator/prices"
+)
 
 func main() {
 	//var prices []float64 = []float64{10,20,30}
-	prices := []float64{10, 20, 30}
+
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 
-		taxIncludedPrices := make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
 	}
 
-	fmt.Println(result)
 }
--- Section-9 (PractiseProject-Price Calculator)/prices/prices.go
+++ Section-9 (PractiseProject-Price Calculator)/prices/prices.go
@@ -1,4 +1,12 @@
-package price
+package prices
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"example.com/price-calculator/conversion"
+)
 
 type TaxIncludedPricesJob struct {
 	TaxRate           float64
@@ -13,13 +21,52 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPricesJob {
 	}
 }
 
-func (job TaxIncludedPricesJob) Process() {
-	result := make(map[string]float64)
-
-	for priceIndex, price := range job.InputPrices {
-		result[priceIndex] = price * (1 + taxRate)
+func (job *TaxIncludedPricesJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("An error occured!")
+		fmt.Println(err)
+		return
 	}
 
-	result[taxRate] = taxIncludedPrices
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("An error occured while reading the txt")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	prices, err := conversion.StringsToFloat(lines)
+
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	job.InputPrices = prices
+	file.Close()
+
+}
+
+func (job *TaxIncludedPricesJob) Process() {
+
+	job.LoadData()
+
+	result := make(map[string]string)
+
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+
+	fmt.Println(result)
 
 }
