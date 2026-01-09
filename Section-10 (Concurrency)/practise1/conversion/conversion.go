6b0df344414d5808c4efb5f24d95ac1875520af8
batuhanbostanci <batuhanbostanci2@hotmail.com>
2026-01-09 16:26:50 +0300

    Starting practise1

---
+++ Section-10 (Concurrency)/practise1/conversion/conversion.go
@@ -0,0 +1,24 @@
+package conversion
+
+import (
+	"errors"
+	"strconv"
+)
+
+func StringsToFloat(strings []string) ([]float64, error) {
+
+	var floats []float64
+
+	for _, string := range strings {
+		floatVal, err := strconv.ParseFloat(string, 64)
+
+		if err != nil {
+			return nil, errors.New("failed to convert to string to float")
+		}
+
+		floats = append(floats, floatVal)
+
+	}
+
+	return floats, nil
+}
