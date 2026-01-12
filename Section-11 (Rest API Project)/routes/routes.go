5e75cae7eb1c3f11d6f162c4ad8b6412ae0c0876
batuhanbostanci <batuhanbostanci2@hotmail.com>
2026-01-12 09:03:09 +0300

    enhancing using the middleware

--- Section-11 (Rest API Project)/routes/routes.go
+++ Section-11 (Rest API Project)/routes/routes.go
@@ -1,14 +1,23 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/rest-api/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 
 	server.GET("/events", getEvents)    // GET,POST, PUT,PATCH,DELETE
 	server.GET("/events/:id", getEvent) // /events/1 //evetns/23
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("events/:id", deleteEvent)
+
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("events/:id", deleteEvent)
+	//this methods runs left to right. so that there is a better way when we dealing with more than one
+	// protected routes. So that we can see the ussage of the on the upper part
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 
